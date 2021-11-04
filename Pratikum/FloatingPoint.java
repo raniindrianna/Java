1223f895d49be179a4cbf0dd3b762a32883e4087
raniindrianna <80735306+raniindrianna@users.noreply.github.com>
2021-11-04 20:17:06 +0700

    typedata

---
+++ Pratikum/FloatingPoint.java
@@ -0,0 +1,11 @@
+
+package operators;
+public class FloatingPoint {
+    public static void main(String[] args) {
+        double x = 92.98;
+        int nx = (int) Math.round(x);
+        
+        System.out.println("Nilai nx adalah : "+Math.round(x));
+    }
+    
+}
